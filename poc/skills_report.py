a2c201ceb7eaae7078933b5637ec69881c9336dc
Andy Green <uayeb.forever@gmail.com>
2019-11-30 15:54:43 +1100

    Simplifying the Jinja2 template tags

--- poc/skills_report.py
+++ poc/skills_report.py
@@ -7,13 +7,13 @@ import crewms
 
 
 latex_jinja_env = jinja2.Environment(
-    block_start_string='\BLOCK{',
-    block_end_string='}',
+    block_start_string='%{',
+    block_end_string='}%',
     variable_start_string='\VAR{',
     variable_end_string='}',
     comment_start_string='\#{',
     comment_end_string='}',
-    line_statement_prefix='%%',
+    # line_statement_prefix='%%',
     line_comment_prefix='%#',
     trim_blocks=True,
     autoescape=False,
